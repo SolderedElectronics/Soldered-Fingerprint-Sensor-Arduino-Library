598f431ea3f8e6c53a131442a32c6dc4e2f4d0a6
Main Push Robot <robot@example.com>
2021-07-28 08:22:01 +0000

    Committing dev changes

---
+++ src/Fingerprint-Sensor-SOLDERED.cpp
@@ -0,0 +1,12 @@
+/**
+ **************************************************
+ *
+ * @file        Generic-easyC-SOLDERED.cpp
+ * @brief       Example functions to overload in base class.
+ *
+ *
+ * @copyright GNU General Public License v3.0
+ * @authors     Zvonimir Haramustek for soldered.com
+ ***************************************************/
+
+// #include "Fingerprint-Sensor-SOLDERED.h"
